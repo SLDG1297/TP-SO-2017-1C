d2a8d896ab94e1eabbcf0533d060c3d3c67b816d
vickynfa <infantino.m.victoria@gmail.com>
2017-05-22 12:11:50 -0300

    Ligeros cambios en la memoria y en el git ignore

--- librerias/funcionesMemoria.h
+++ librerias/funcionesMemoria.h
@@ -8,17 +8,27 @@
 #ifndef FUNCIONESMEMORIA_H_
 #define FUNCIONESMEMORIA_H_
 
+
 #include <netinet/in.h>
 #include <commons/log.h>
 #include <string.h>
 
+
 #include "../librerias/controlArchivosDeConfiguracion.h"
-#include "../librerias/funcionesMemoria.h"
-#include "../librerias/operacionesMemoria.h"
+//#include "../librerias/funcionesMemoria.h"
 
 #define RUTA_ARCHIVO "./config_memoria.cfg"
-#define PROCESO_VACIO -14
-#define PAG_LIBRE -7
+#define LIBRE -7
+#define OCUPADO_x_STR -14
+#define NOT_FOUND -28
+
+//DECLARACION Y ASIGNACION DE DATOS PARA EL ARCHIVO DE CONFIGURACION
+int PUERTO_MEMORIA;
+int FRAME_SIZE;
+int FRAMES;
+int ENTRADAS_CACHE;
+int CACHE_X_PROC;
+int RETARDO_MEMORIA;
 
 t_log *archivoLog;
 
@@ -28,52 +38,61 @@ typedef struct {
 	int frame;
 }strAdministrativa;
 
-void asignarDatosDeConfiguracion(int* PUERTO_MEMORIA, int* MARCO_SIZE, int* MARCOS, int* ENTRADAS_CACHE, int* CACHE_X_PROC, int* RETARDO_MEMORIA){
+
+void asignarDatosDeConfiguracion(){
 	t_config* configuracion;
 	char* ruta = RUTA_ARCHIVO;
 	configuracion = llamarArchivo(ruta);
 
-	*PUERTO_MEMORIA = busquedaClaveNumerica(configuracion, "PUERTO");
-	*MARCO_SIZE = busquedaClaveNumerica(configuracion, "MARCO_SIZE");
-	*MARCOS = busquedaClaveNumerica(configuracion, "MARCOS");
-	*ENTRADAS_CACHE = busquedaClaveNumerica(configuracion, "ENTRADAS_CACHE");
-	*CACHE_X_PROC = busquedaClaveNumerica(configuracion, "CACHE_X_PROC");
-	*RETARDO_MEMORIA = busquedaClaveNumerica(configuracion, "RETARDO_MEMORIA");
+	PUERTO_MEMORIA = busquedaClaveNumerica(configuracion, "PUERTO");
+	FRAME_SIZE = busquedaClaveNumerica(configuracion, "MARCO_SIZE");
+	FRAMES = busquedaClaveNumerica(configuracion, "MARCOS");
+	ENTRADAS_CACHE = busquedaClaveNumerica(configuracion, "ENTRADAS_CACHE");
+	CACHE_X_PROC = busquedaClaveNumerica(configuracion, "CACHE_X_PROC");
+	RETARDO_MEMORIA = busquedaClaveNumerica(configuracion, "RETARDO_MEMORIA");
+}
+
+//Reservo la memoria que necesito
+int* reservarMemoria(){
+	int* memoria;
+	memoria=malloc(FRAME_SIZE * FRAMES);
+	if (memoria == NULL)
+		esErrorConSalida(-1,"Error en la solicitud de bloque de memoria (Malloc)");
+	return memoria;
 }
 
 //Seteo todas las estructuras con el valor predefinido PID=-14 de esta manera puedo saber que el frame no tiene un proceso asignado
-void inicializarStrAdm(int FRAMES, int FRAME_SIZE, int* bloqueMemoria){
+void inicializarStrAdm(int* bloqueMemoria){
 
-	//El tamaño de la estructura por la cantidad de FRAMES 500*256B = 6000 B
-	//Si hacemos 6000/MARCO_SIZE nos da 23,43, por lo cual le sumamos un marco para que asi nos de 24.
-	//De esta manera tenemos 24 FRAMES ocupados por la memoria administrativa
 	int sizeStrAdm;
 	int sizeBlockAdm;
 	int contador_frame = 0; // contador para los frames dentro del for
 	strAdministrativa auxiliar;
 
+
+	//El tamaño de la estructura por la cantidad de FRAMES 500*256B = 6000 B
+	//Si hacemos 6000/MARCO_SIZE nos da 23,43, por lo cual le sumamos un marco para que asi nos de 24.
+	//De esta manera tenemos 24 FRAMES ocupados por la memoria administrativa
 	sizeStrAdm = sizeof(strAdministrativa);
-	sizeBlockAdm = ((sizeStrAdm*FRAMES)+FRAME_SIZE-1)/FRAMES;
+	sizeBlockAdm = ((sizeStrAdm*FRAMES)+FRAME_SIZE)/FRAMES;
 
-	auxiliar.pid=PROCESO_VACIO;
+	auxiliar.pid=LIBRE;
 	auxiliar.frame=contador_frame;
-	auxiliar.nroPagina=PAG_LIBRE;
+	auxiliar.nroPagina=LIBRE;
 
+	//mientras la cantidad de frames del contador sea menor a 500 (FRAMES)
 	while(contador_frame<FRAMES){
-
-		//inicializo los valores del frame del bloque administrativo de esta manera despues podemos distinguir un frame vacio de uno ocupado
-		if(contador_frame<sizeBlockAdm){
-			auxiliar.pid=PROCESO_VACIO;
+		//Con este if nos aseguramos que hay 12 frames utilizados para las estructuras administrativas
+		//de esta manera cuando guardemos info no vamos a pisar nada
+		if(contador_frame>=FRAMES-sizeBlockAdm){
+			auxiliar.pid=OCUPADO_x_STR;
 			auxiliar.nroPagina=contador_frame;
 		}
 		else{
-			auxiliar.pid=PAG_LIBRE;
-			auxiliar.nroPagina=PAG_LIBRE;
+		auxiliar.pid=LIBRE;
+		auxiliar.nroPagina=LIBRE;
 		}
-
-
-
-		//Esta funcion copia los datos The memcpy() function copies n bytes from memory area src to memory area dest.
+		//Esta funcion copia los datos The memcpy(DEST,SRC,BYTES) function copies n bytes from memory area src to memory area dest.
 		memcpy(bloqueMemoria+contador_frame*sizeStrAdm,&auxiliar,sizeStrAdm);
 		contador_frame++;
 		auxiliar.frame=contador_frame;
@@ -83,26 +102,99 @@ void inicializarStrAdm(int FRAMES, int FRAME_SIZE, int* bloqueMemoria){
 	log_info(archivoLog,"Son %d estructuras con un peso individual de %d",FRAMES,sizeStrAdm);
 }
 
-void crearArchivo(char* ruta){
+
+t_log* crearArchivo(char* ruta){
 
 	archivoLog = log_create(ruta,"Memoria",true,LOG_LEVEL_INFO);
+	return archivoLog;
+}
+
+void iniciarStrAdmProg(int pid, int frames, int* memoria){
+
+	strAdministrativa aux;
+
+
+
+}
+
+void imprimirStrAdm(int* bloqueMemoria){
+	strAdministrativa aux;
+	int c=0;
+	while(c<FRAMES){
+		memcpy(&aux,bloqueMemoria+c*sizeof(strAdministrativa),sizeof(strAdministrativa));
+		printf("\nPID: %d, NroPagina: %d, Frame: %d",aux.pid,aux.nroPagina,aux.frame);
+		//log_info(archivoLog,"\n*******************************************\n");
+		//log_info(archivoLog,"PID: %d, NroPagina: %d, Frame: %d",aux.pid,aux.nroPagina,aux.frame);
+		c++;
+	}
+}
+
+//La estructura se encuentra libre siempre y cuando su PID sea igual a -7
+int strLibre(int* ptr){
+	strAdministrativa aux;
+	memcpy(&aux,ptr,sizeof(strAdministrativa));
+	if(aux.pid==LIBRE)
+		return 1;
+	else
+		return 0;
+
 }
 
 
 
 
+void liberarStrAdm(int pid,int* ptr){
+	int c=-1;
+	strAdministrativa aux;
+	do{
+		c++;
+		memcpy(&aux,ptr+c*sizeof(strAdministrativa),sizeof(strAdministrativa));
+	}while(c<FRAMES&&aux.pid!=pid);
 
+	aux.pid=LIBRE;
+	aux.nroPagina=LIBRE;
+	aux.frame=c;
+	memcpy(ptr+c*sizeof(strAdministrativa),&aux,sizeof(strAdministrativa));
 
+}
+int getPuertoMemoria(){
+	return PUERTO_MEMORIA;
+}
+t_log* getArchivoLog(){
+	return archivoLog;
+}
 
+int buscarPID(int pid, int* memoria){
+	strAdministrativa aux;
+	int countFrame=-1;
+	do{
+		countFrame++;
+		memcpy(&aux,memoria+countFrame*sizeof(strAdministrativa),sizeof(strAdministrativa));
+		if(aux.pid==pid)
+			return aux.frame;
+	}while(countFrame<FRAMES);
 
+	return NOT_FOUND;
+}
 
+int buscarStrAdm(int pid, int* memoria){
+	strAdministrativa aux;
+		int countFrame=-1;
+		do{
+			countFrame++;
+			memcpy(&aux,memoria+countFrame*sizeof(strAdministrativa),sizeof(strAdministrativa));
+			if(aux.pid==pid)
+				return countFrame;
+		}while(countFrame<FRAMES);
 
+	return NOT_FOUND;
 
+}
 
 //Modificar la cantidad de tiempo que espera el proceso memoria antes de responder una solicitud
 void retardo (int tiempoEspera){}
 
-//Genera un reporte en pantalla y en un archivo en disco del estado actual del 
+//Genera un reporte en pantalla y en un archivo en disco del estado actual del
 //cache, estructuras de memoria, contenido de memoria
 void dump (){}
 
@@ -113,6 +205,8 @@ void flush (int *ptrCache){}
 //PID: Indicara el tamaño total de un procoeso
 void size (){}
 
+void recibir (){}
 
+void enviar (int* socket, char* buffer ){}
 
 #endif /* FUNCIONESMEMORIA_H_ */
--- memoria/memoria.c
+++ memoria/memoria.c
@@ -34,7 +34,7 @@
 #include "../librerias/controlArchivosDeConfiguracion.h"
 #include "../librerias/controlErrores.h"
 #include "../librerias/funcionesMemoria.h"
-#include "../librerias/operacionesMemoria.h"
+
 
 #define RUTA_ARCHIVO "./config_memoria.cfg"
 #define SIZE_DATA 1024
@@ -42,90 +42,70 @@
 
 //DECLARACION DE VARIABLES PARA VALORES DE RESPUESTA
 
-//*Socket para esperar conexion con el kernel
-int socketEsperaKernel;
-int bytesRecibidos;
 
 
 //*Socket para entablar conexion con el kernel
 int sockAlKernel;
 int yes = 1;
 struct sockaddr_in espera, datosDelKernel;
+int socketServidor;
 
-//DECLARACION Y ASIGNACION DE DATOS PARA EL ARCHIVO DE CONFIGURACION
+//*Puntero a memoria contigua
+int *ptrMemoria;
 
-int puertoMemoria;
-int frameSize;
-int frames;
-int entradasCache;
-int cache_x_proc;
-int retardoMemoria;
+t_log* archivoLog;
 
 //DECLARACION DE PROTOTIPO DE FUNCIONES
-
 
 int crearSocket();
 void laConexionFueExitosa(char* handshake);
 void iniciarConexion();
-
 
 //************** MAIN **************//
 int main(int argc, char *argv[]) {
 
 //DECLARACION DE VARIABLES PARA EL CODIGO PRINCIPAL
 
-
 //*Socket para entablar conexion con el kernel
 	char buffer[SIZE_DATA];
-	char* handshake = "La conexion al proceso memoria fue exitosa";
+//	char* handshake = "La conexion al proceso memoria fue exitosa";
 	memset(buffer, '\0', SIZE_DATA);
 
-	//*Puntero a memoria contigua
-	int *ptrMemoria;
 
 
 // CODIGO PRINCIPAL DE LA CONSOLA
 	//*solicitud de bloque de memoria contigua
-	asignarDatosDeConfiguracion(&puertoMemoria, &frameSize, &frames,
-			&entradasCache, &cache_x_proc, &retardoMemoria);
+	asignarDatosDeConfiguracion();
 
-	ptrMemoria = malloc(frameSize * frames);
-	if (ptrMemoria == NULL)
-		esErrorConSalida(-1,
-				"Error en la solicitud de bloque de memoria (Malloc)");
-	crearArchivo(RUTA_LOG);
-	inicializarStrAdm(frames, frameSize,&ptrMemoria);
+
+	ptrMemoria =reservarMemoria();
+	archivoLog=crearArchivo(RUTA_LOG);
+	inicializarStrAdm(ptrMemoria);
+	imprimirStrAdm(ptrMemoria);
+
+	socketServidor=iniciarConexionServidor();
+
+
+
 	//*asignacion de datos al socket que espera a la conexion del kernel
 
 	//DIRECCION PARA ESPERAR A LA CONEXION CON EL KERNEL
-	iniciarConexion();
+	/*iniciarConexion();
 
-	laConexionFueExitosa(handshake);
+	 laConexionFueExitosa(handshake);
 
 
-	puts("La conexion al proceso kernel fue exitosa\n");
-	puts("Esperando mensajes\n");
-
-//	while (1) {
-//
-//		bytesRecibidos = recv(sockAlKernel, buffer, sizeof(buffer), 0);
-//		esErrorSinSalida(bytesRecibidos, "Fallo en la recepcion");
-//
-//		sinBytesRecibidos(bytesRecibidos);
-//
-//		printf("Buffer: %s\n", buffer);
-//		memset(buffer, '\0', SIZE_DATA);
-//	}
-
+	 puts("La conexion al proceso kernel fue exitosa\n");
+	 puts("Esperando mensajes\n");
+	 */
 
 	return 0;
 
 }
 
-void iniciarConexion() {
+int iniciarConexionServidor() {
 
-
-	socketEsperaKernel = crearSocket();
+	int socketEsperaKernel = crearSocket();
 	printf("Setsockopt correcto\n");
 	puts("Socket de espera creado\n");
 
@@ -136,49 +116,101 @@ void iniciarConexion() {
 	int longitudEstructuraSocket = sizeof(datosDelKernel);
 	//DIRECCION PARA ESPERAR A LA CONEXION CON EL KERNEL
 
-
 	espera.sin_family = AF_INET;
-	espera.sin_port = htons(puertoMemoria);
+	espera.sin_port = htons(getPuertoMemoria());
 	espera.sin_addr.s_addr = INADDR_ANY;
 	bzero(&(espera.sin_zero), 8);
 
 //Bind(): Asigna la direccion de addr al socket referido por el SocketEsperaKernel
 	valorRtaBind = bind(socketEsperaKernel, (struct sockaddr *) &espera,
 			sizeof(struct sockaddr));
-	errorSalidaSocket(valorRtaBind, "Error en Bind",&socketEsperaKernel);
+	errorSalidaSocket(valorRtaBind, "Error en Bind", &socketEsperaKernel);
 
 	printf("Bind correcto\n");
 	puts("Esperando la conexion del kernel\n");
 
 //Listen(): Marca al socket como pasivo, es decir un socket que sera utilizado para aceptar conecciones
 	valorRtaListen = listen(socketEsperaKernel, 5);
-	errorSalidaSocket(valorRtaListen, "Error en Listen",&socketEsperaKernel);
+	errorSalidaSocket(valorRtaListen, "Error en Listen", &socketEsperaKernel);
 
 //Accept(): Extrae el primer pedido de conexion de la cola de conexiones pendientes, del socket de escucha
 //crea un nuevo socket conecatado y retorna un nuevo descriptor de archivo referido a ese socket.
-	sockAlKernel = accept(socketEsperaKernel, (struct sockaddr *) &datosDelKernel,
-			&longitudEstructuraSocket);
-	errorSalidaSocket(sockAlKernel, "Error en el accept",&socketEsperaKernel);
+	sockAlKernel = accept(socketEsperaKernel,
+			(struct sockaddr *) &datosDelKernel, &longitudEstructuraSocket);
+	errorSalidaSocket(sockAlKernel, "Error en el accept", &socketEsperaKernel);
+
+	return sockAlKernel;
 }
-int crearSocket(){
+int crearSocket() {
 	//Declaracion de variable para recibir valor de rta
 	int valorRtaSetSockOpt;
 	int sockDeEspera;
 
 	//Socket(): creates an endpoint for communication and returns a file descriptor that refers to that endpoint.
 
-	sockDeEspera =socket(AF_INET, SOCK_STREAM, 0);
-	errorSalidaSocket(sockDeEspera, "Fallo en la creacion del socket de espera",&sockDeEspera);
+	sockDeEspera = socket(AF_INET, SOCK_STREAM, 0);
+	errorSalidaSocket(sockDeEspera, "Fallo en la creacion del socket de espera",
+			&sockDeEspera);
 
 	valorRtaSetSockOpt = setsockopt(sockDeEspera, SOL_SOCKET, SO_REUSEADDR,
-				(const char*)&yes, sizeof(yes));
+			(const char*) &yes, sizeof(yes));
 	errorSalidaSocket(valorRtaSetSockOpt,
-				"Error en setSockOpt en el socket de espera",&sockDeEspera);
+			"Error en setSockOpt en el socket de espera", &sockDeEspera);
 	return sockDeEspera;
 }
-void laConexionFueExitosa(char* handshake){
+void laConexionFueExitosa(char* handshake) {
+
 	int longitudDatosEnviados;
 	longitudDatosEnviados = send(sockAlKernel, handshake, strlen(handshake), 0);
 	esErrorSinSalida(longitudDatosEnviados, "Fallo en el handshake");
 
 }
+
+
+// ************************* OPERACIONES DE MEMORIA *************************************
+
+/*
+ * Cuando el KRN comunique el inicio de un nuevo prog se crearan las estructuras necesarias para administrarlo correctamente. En una misma pag
+ * no se podran tener datos referentes a dos segmentos diferentes
+ */
+
+int inicializarPrograma(int pid, int cantPaginas){
+
+	iniciarStrAdmProg(pid, cantPaginas,ptrMemoria);
+
+	return 0;
+}
+
+
+/*
+ * Ante un pedido de lectura de paginas de alguno de los PRCs de la CPU, se realizara la traduccion a MARCO/FRAME y se devolvera el contenido
+ * correspondiente consultado primeramente a la M. Cache, en caso de que esta no contenga la info se informara un Cache Miss, se debera cargar
+ * la pagina en cache y se devolvera la informacion solicitada
+ */
+//char* solicitarBytesDePagina(int pid, int nroPagina, int offset, int size ){}
+
+/*
+ * Ante un pedido de escritura de paginas de alguno de los PRCDs, se realizara la traduccion a MARCO/FRAME y se actualizara su contenido
+ * En caso de que la pag se encuentr en la M. Cache se debera actualizar tambien el frame alojado en la misma
+ */
+//int almacenarBytesEnPagina(int pid, int nroPagina, int offset, int size, char* buffer){}
+
+
+/*
+ *El PRC memoria debera aignarle tantas paginas como se soliciten, al PRC ampliando asi su tamaño. En caso de que no se
+ *pueda asignar ese espacio, se debera informar al KRN la falta de espacio
+ */
+//int asignarPaginasAProceso(int pid, int paginasNecesarias){}
+
+
+//Cuando el PRC KRN informe el fin de un prog. se deben eliminar las entradas en estructuras usadas de memoria
+int finalizarPrograma (int pid){
+
+ liberarStrAdm(pid,ptrMemoria);
+ printf("\nEl programa %d ha finalizado",pid);
+ return 0;
+}
+
+
+
+
