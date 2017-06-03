d67c8af9875c46bf7de8edec73fce6489cc7cd1f
Erik-1012 <bernaderi@hotmail.com>
2017-06-03 09:29:57 -0300

    Arreglos + dos primitivas

--- librerias/primitivasParser.h
+++ librerias/primitivasParser.h
@@ -24,19 +24,49 @@ u_int32_t* TAMANIO_PAG;
 //Funcion para tener cargado el pcb
 void llenarVarsPrimitivas(pcb* _PCB, u_int32_t* _TAMANIO_PAG);
 
+//Funcion para posicionarme en la última posición del stack del pcb
+indiceDeStack* pararmeEnContextoActual();
+
+//Se le pide espacio a la memoria para una variable de tamaño tamañoDeVariable
+posicionMemoria solicitarEspacioAMemoria(u_int32_t pid, u_int32_t tamañoDeVariable);
+
+//Se le pide el valor a la memoria dándole su página, offset y size junto con el pid del proceso;
+t_valor_variable solicitarValorAMemoria(solicitudMemoria solicitud);
+
+//Escribe el valor pedido en determinada posicion de memoria dandole el pid del proceso
+void escribirValorEnMemoria(solicitud, valor);
+
+//Le pide al kernel el valor de la variable compartida solicitada
+t_valor_variable pedirKernelVariableCompartida(variable);
+
+//Se le pide al Kernel asignar un valor a determinada variable compartida
+void asignarKernelVariableCompartida(variable,valor);
+
 //Funcion para calcular el t_puntero a partir de una posicion de memoria
 t_puntero calculart_puntero(posicionMemoria posMem);
 
 //Dado una dirección como numPag*TAMAÑO_PAG + offset calcula buscando en el stack(cualquier contexto) su correspondiente posicion de memoria
 posicionMemoria* obtenerPosicionMemoria(t_puntero direccion);
 
+//Esta función debe avisarle al Kernel que terminó la ejecución del programa
+void finalizarPrograma();
+
 void llenarPCB(pcb* _PCB, u_int32_t* _TAMANIO_PAG){
 	PCB = _PCB;
 	TAMANIO_PAG = _TAMANIO_PAG;
 }
 
+indiceDeStack* pararmeEnContextoActual(){
+	t_list* stack = PCB->indiceStack;
+
+	//Indice de la última posición para pararme en el contexto actual
+	int ultPos = (stack->elements_count) - 1;
+
+	return (indiceDeStack*)list_get(stack,ultPos);
+}
+
 t_puntero calculart_puntero(posicionMemoria posMem){
-	u_int32_t tamanioPag = *TAMANIO_PAG; // Te pongo esto porque lo otro era antipedagógico: confundía punteros con un producto XD
+	u_int32_t tamanioPag = *TAMANIO_PAG;
 	return (posMem.pagina)*tamanioPag + posMem.offset;
 }
 
@@ -65,8 +95,8 @@ posicionMemoria* obtenerPosicionMemoria(t_puntero direccion){
     variableStack* varStack = (variableStack*)list_get(aux->variables,k);
 
     //Si encontró la dirección correcta
-    if(direccion == calculart_puntero(*varStack)){ // Estás flasheando polimorfismo paramétrico XD Ahí va posicionMemoria, no varStack XD
-        	return (posicionMemoria*)varStack;
+    if(direccion == calculart_puntero(varStack->posicionMemoria)){
+        	return varStack->posicionMemoria;
     }
     }/*Fin del for de lista de variables*/
 
@@ -98,14 +128,16 @@ t_valor_variable obtenerValorCompartida(t_nombre_compartida variable);
 t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor);
 
 //Cambia la linea de ejecucion a la correspondiente de la etiqueta buscada
-void irAlLabel(t_nombre_etiqueta t_nombre_etiqueta);
+void irAlLabel(t_nombre_etiqueta nombre_etiqueta);
 
 void llamarSinRetorno(t_nombre_etiqueta etiqueta);
 
 void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
 
+//Cambia el contexto al anterior, actualizando el PC. En caso de finalizar el programa, se deberá notificar.
 void finalizar(void);
 
+//Se debe almacenar el valor de la variable retorno en la posición de memoria indicada en el stack en retVar.
 void retornar(t_valor_variable retorno);
 
 
@@ -114,12 +146,10 @@ void retornar(t_valor_variable retorno);
 //-------------------------------------------------------------------------Falta considerar lo del diccionario de variables
 t_puntero definirVariable(t_nombre_variable identificador_variable){
 
-	posicionMemoria posMem /*= solicitarEspacioAMemoria(tamañoDeVariable)*/;
-	t_list* stack = PCB->indiceStack;
-	//Indice de la última posición para pararme en el contexto actual
-	int ultPos = (stack->elements_count) - 1;
+	u_int32_t tamañoDeVariable = sizeof(identificador_variable);
+	posicionMemoria posMem = solicitarEspacioAMemoria(tamañoDeVariable); //-------------------------------TODO
 
-	indiceDeStack* aux = (indiceDeStack*)list_get(stack,ultPos);
+	indiceDeStack* aux = pararmeEnContextoActual();
 
 	//Para ver si es el argumento de una funcion
 	if (identificador_variable >= '0' && identificador_variable <= '9'){
@@ -141,13 +171,19 @@ t_puntero definirVariable(t_nombre_variable identificador_variable){
 
 t_puntero obtenerPosicionVariable(t_nombre_variable identificador_variable){
 
-	t_list* stack = PCB->indiceStack;
-	//Indice de la última posición para pararme en el contexto actual
-	int ultPos = (stack->elements_count) - 1;
+	indiceDeStack* aux = pararmeEnContextoActual();
 
-	indiceDeStack* aux = (indiceDeStack*)list_get(stack,ultPos);
+	//Primero busco en la lista de argumentos
+
+	//Para ver si es el argumento de una funcion
+	if (identificador_variable >= '0' && identificador_variable <= '9'){
+	t_list* listaArgumentos = aux->argumentos;
+	argStack* argStack =(argStack*)list_get(listaArgumentos,identificador_variable);
+	return calculart_puntero((posicionMemoria)(*argStack));
+	}
 
 	//Ahora tengo que recorrer la lista de variables hasta encontrar la requerida
+
 	t_list* listaVariables = aux->variables;
 	t_list* pr = listaVariables;
 	while(((variableStack*)pr->head->data)->nombre != identificador_variable && pr->head->next != NULL){
@@ -160,41 +196,77 @@ t_puntero obtenerPosicionVariable(t_nombre_variable identificador_variable){
 		return calculart_puntero(posMem);
 	}
 
-	//Si no estaba la variable en la lista
+	//Si no estaba la variable en niguna de las listas
     return -1;
 }
 
 t_valor_variable dereferenciar(t_puntero direccion_variable){
 
 	posicionMemoria* posMem = obtenerPosicionMemoria(direccion_variable);
-    u_int32_t pagina = posMem->pagina;
-    u_int32_t offset = posMem->offset;
-    u_int32_t size = posMem->size;
-    t_valor_variable valor /*= solicitarMemoria(pagina, offset, size)*/;
+	solicitudMemoria solicitud;
+	solicitud.pid = PCB->pid;
+	solicitud.pagina = posMem->pagina;
+	solicitud.offset = posMem->offset;
+	solicitud.size = posMem->size;
+    t_valor_variable valor = solicitarValorAMemoria(solicitud); //-----------------------------------------TODO
     return valor;
 }
 
 void asignar(t_puntero direccion_variable, t_valor_variable valor){
 	posicionMemoria* posMem = obtenerPosicionMemoria(direccion_variable);
-	u_int32_t pagina = posMem->pagina;
-	u_int32_t offset = posMem->offset;
-	u_int32_t size = posMem->size;
+	solicitudMemoria solicitud;
+	solicitud.pid = PCB->pid;
+	solicitud.pagina = posMem->pagina;
+	solicitud.offset = posMem->offset;
+	solicitud.size = posMem->size;
 	//Se le pide escritura a la memoria en la posicion determinada
-	//escribirValorEnMemoria(pagina, offset, size, valor);
+	escribirValorEnMemoria(solicitud, valor); //----------------------------------------------------------TODO
 }
 
 t_valor_variable obtenerValorCompartida(t_nombre_compartida variable){
-	t_valor_variable valor /*= pedirKernelVariableCompartida(variable)*/;
+	t_valor_variable valor = pedirKernelVariableCompartida(variable); //----------------------------------TODO
 	return valor;
 }
 
 t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor){
-	t_valor_variable valorAsignado /*= asignarKernelVariableCompartida(variable,valor)*/;
+	t_valor_variable valorAsignado = asignarKernelVariableCompartida(variable,valor); //-------------------TODO
 	return valorAsignado;
 }
 
-void irAlLabel(t_nombre_etiqueta t_nombre_etiqueta){
+void irAlLabel(t_nombre_etiqueta nombre_etiqueta){
 
+}
+
+void finalizar(){
+
+	t_list* stack = PCB->indiceStack;
+
+	//Indice de la última posición para pararme en el contexto actual
+	int ultPos = (stack->elements_count) - 1;
+
+	indiceDeStack* stackActual = pararmeEnContextoActual();
+
+	//Se sale del contexto de una función
+    if(ultPos != 0){
+    	u_int32_t newPC = stackActual->retPos;
+    	PCB->programCounter = newPC;
+    }
+
+    //En cualquier caso se elimina el contexto actual del stack
+    list_remove(stackActual,ultPos);
+
+    //Finalizó el programa
+    if(ultPos == 0){
+    	finalizarPrograma(); //------------------------------------------------------------------------TODO
+    }
+}
+
+void retornar(t_valor_variable retorno){
+
+	t_puntero posicion = obtenerPosicionVariable(retorno);
+	t_valor_variable valor = dereferenciar(posicion);
+	indiceDeStack* stackActual = pararmeEnContextoActual();
+    asignar(calculart_puntero(stackActual->retVar),valor);
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
